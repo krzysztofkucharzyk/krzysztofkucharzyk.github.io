2bd0b184c6f436b88b5f5016a998d23db6f1b294
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-08-17 21:41:48 +0200

    bez listy

--- DOM/wyplaty-premie-dodatkowe/js/script.js
+++ DOM/wyplaty-premie-dodatkowe/js/script.js
@@ -1,26 +1,35 @@
+let button = document.getElementById('oblicz');
+
+let pracownik = document.getElementsByClassName('pracownik');
 let czas = document.getElementsByClassName('czas');
 let stawka = document.getElementsByClassName('stawka');
+let wyplata = document.getElementsByClassName('wyplata');
 
-for (let i = 0; i < czas.length; i++) {
-    for (let j = 0; j < stawka.length; j++) {
+const przeliczWyplaty = () => {
 
-        console.log(parseInt(czas[i].value) + parseInt(stawka[j].value));
-        
+    for (let i = 0; i < czas.length; i++) {
+
+        let wyplataRazem = (parseInt(czas[i].value) * parseInt(stawka[i].value));
+        let premia = ((parseInt(czas[i].value) - 160) * (parseInt(stawka[i].value) * 2));
+        let wyplataPremia = wyplataRazem + premia;
+
+        if (parseInt(czas[i].value) < 100) {
+            pracownik[i].style.backgroundColor = 'red';
+        }
+
+        if (parseInt(czas[i].value) <= 160) {
+            wyplata[i].innerHTML = `${wyplataRazem} PLN`;
+        } else {
+            wyplata[i].innerHTML = `${wyplataPremia} PLN (premia: ${premia} PLN)`;
+        }
     }
-    
 }
 
 
-// for (let i = 0; i < czas.length; i++) {
-//     for (let j = 0; j < stawka.length; j++) {
-
-//         let sum = parseInt(czas[i].value) + parseInt(stawka[j].value);
-//         document.getElementsByClassName('wyplata').value = sum;
-//         // console.log(czas[i].value);
-        
-//     }
-    
-// }
 
 
 
+
+
+
+button.addEventListener('click', przeliczWyplaty);
