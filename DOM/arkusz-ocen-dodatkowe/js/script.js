2079dde90ffe59166dae8c0508d3fb2c9ae9ad79
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-08-21 17:04:35 +0200

    nieudana próba

--- DOM/arkusz-ocen-dodatkowe/js/script.js
+++ DOM/arkusz-ocen-dodatkowe/js/script.js
@@ -1,5 +1,6 @@
-let uczen = document.querySelectorAll("[id^='uczen']");
-// let input = document.querySelectorAll('input[type="number"]');
+let uczenId = document.querySelectorAll("[id^='uczen']");
+let input = document.querySelectorAll('input[type="number"]');
+let uczen = document.getElementsByClassName('uczen');
 let mat = document.getElementsByClassName('matematyka');
 let pol = document.getElementsByClassName('polski');
 let bio = document.getElementsByClassName('biologia');
@@ -8,20 +9,79 @@ let fiz = document.getElementsByClassName('fizyka');
 let che = document.getElementsByClassName('chemia');
 let inf = document.getElementsByClassName('informatyka');
 let sredniaInput = document.getElementsByClassName('srednia');
-// let sredniaInput = document.querySelectorAll('span[class="srednia"]');
-// let sredniaInput = document.querySelectorAll("[id^='uczen']").getElementsByClassName('srednia');
 let dodatkowe = document.getElementsByClassName('zajecia-dodatkowe');
 
-for (i = 0; i < uczen.length + 1; i++) {
+let button = document.getElementById('oblicz');
 
-    let sredniaOcen = [parseFloat(mat[i].value) + parseFloat(pol[i].value) + parseFloat(bio[i].value) + parseFloat(geo[i].value) + parseFloat(fiz[i].value) + parseFloat(che[i].value) + parseFloat(inf[i].value)] / 7;
+let div = document.querySelector("[id^='uczen']");
+let divInput = div.querySelectorAll('input[type="number"]');
+let divInputLen = div.querySelectorAll('input[type="number"]').length;
+let divInputValue = div.querySelectorAll('input[type="number"]')[0].value;
 
-    let valueRound = sredniaOcen.toFixed(1);
-
-    sredniaInput[i].innerHTML= valueRound;
+const liczenieSredniej = (event) => {
     
+    for (i = 0; i < uczenId.length + 1; i++) {
+
+        let sredniaOcen = [parseFloat(mat[i].value) + parseFloat(pol[i].value) + parseFloat(bio[i].value) + parseFloat(geo[i].value) + parseFloat(fiz[i].value) + parseFloat(che[i].value) + parseFloat(inf[i].value)] / divInputLen;
+
+        sredniaInput[i].innerHTML = sredniaOcen.toFixed(2);
+
+        if (parseFloat(sredniaInput[i].value) < 4,75) {
+            uczen[i].style.backgroundColor = 'red';
+            // pracownik[i].style.color = 'white';
+        }
 
 
-    console.log(valueRound);
+        
+        
+        const podwyzszenieOceny = 0.5;
+        
+        // if (!(dodatkowe[i].value == "") && sredniaOcen < 6) {
+        //     let ocenaDodatkowa = sredniaOcen + podwyzszenieOceny;
+        //     sredniaInput[i].innerHTML = ocenaDodatkowa.toFixed(1);
+            
+        // }
+
+        if (dodatkowe[i].value === 'matematyka' && parseFloat(mat[i].value) < 6) {
+            let matPodwyzszona = parseFloat(mat[i].value) + podwyzszenieOceny;
+            mat[i].value = matPodwyzszona.toFixed(1);
+        }
+
+        if (dodatkowe[i].value === 'polski' && parseFloat(pol[i].value) < 6) {
+            let polPodwyzszona = parseFloat(pol[i].value) + podwyzszenieOceny;
+            pol[i].value = polPodwyzszona.toFixed(1);
+        }
+
+        if (dodatkowe[i].value === 'biologia' && parseFloat(bio[i].value) < 6) {
+            let bioPodwyzszona = parseFloat(bio[i].value) + podwyzszenieOceny;
+            bio[i].value = bioPodwyzszona.toFixed(1);
+        }
+
+        if (dodatkowe[i].value === 'geografia' && parseFloat(geo[i].value) < 6) {
+            let geoPodwyzszona = parseFloat(geo[i].value) + podwyzszenieOceny;
+            geo[i].value = geoPodwyzszona.toFixed(1);
+        }
+
+        if (dodatkowe[i].value === 'fizyka' && parseFloat(fiz[i].value) < 6) {
+            let fizPodwyzszona = parseFloat(fiz[i].value) + podwyzszenieOceny;
+            fiz[i].value = fizPodwyzszona.toFixed(1);
+        }
+
+        if (dodatkowe[i].value === 'chemia' && parseFloat(che[i].value) < 6) {
+            let chePodwyzszona = parseFloat(che[i].value) + podwyzszenieOceny;
+            che[i].value = chePodwyzszona.toFixed(1);
+        }
+
+        if (dodatkowe[i].value === 'informatyka' && parseFloat(inf[i].value) < 6) {
+            let infPodwyzszona = parseFloat(inf[i].value) + podwyzszenieOceny;
+            inf[i].value = infPodwyzszona.toFixed(1);
+        }
+
+        
+        
+
+    }
 
 }
+
+button.addEventListener('click', liczenieSredniej);
