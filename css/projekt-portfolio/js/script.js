65e27f463b02c01122f4cb6cbb2a175840d41370
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-09-02 17:07:22 +0200

    smooth-scroll added in jQuery

--- css/projekt-portfolio/js/script.js
+++ css/projekt-portfolio/js/script.js
@@ -6,3 +6,11 @@ function menuToggle() {
     let nav = document.getElementById('nav-toggle');
     nav.classList.toggle('show');
 }
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
