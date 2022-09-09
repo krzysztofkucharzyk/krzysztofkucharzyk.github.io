6adb0eb79048cde556ac95489cd9e1fc35a189c5
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-09-09 10:03:02 +0200

    testy

--- AJAX/kurs-bitcoin-click-dodatkowe/js/script.js
+++ AJAX/kurs-bitcoin-click-dodatkowe/js/script.js
@@ -1,29 +1,82 @@
 $(document).ready(function(){
 
+let kursBtc = 0;
+
    $('#btn').click(function(){
     $.getJSON('https://api.blockchain.com/v3/exchange/tickers')
         .done(function(data){
-            let storedValue = [];
-            let btc = $.grep(data, function(v){return v.symbol === 'BTC-USD'}); 
-                
-            // console.log(btc);
-
-                $.each(btc, function(key, value){
-                    let currentValue = value.last_trade_price;
-                    storedValue.push(currentValue);
-                    // let old = storedValue.findIndex(x => x === currentValue) - 1;
-                    
-
-
-                    console.log(storedValue);
-                    console.log(currentValue);
-                    console.log(btc);
-                    console.log(value);
-                    
-                    console.log(storedValue[old]);
-                });
-                
             
+            let btc = $.grep(data, function(v){return v.symbol === 'BTC-USD'}); // Tworzy tablice
+            
+            let ostatniaWartosc = btc[0].last_trade_price;
+
+            console.log(btc[0].last_trade_price);
+            console.log(btc);
+
+            if($('p')) {
+
+                $("p").text(ostatniaWartosc);
+                console.log('paragraf istnieje');
+            // $('.btcLow').append(low);
+            } else {
+                let high = $("<p></p>").text(ostatniaWartosc);
+        
+                $('.btcHigh').append(high);
+            }
+
+            
+
+            if (kursBtc === ostatniaWartosc) {
+                console.log('kurs jest równy');
+            } else if ( kursBtc > ostatniaWartosc) {
+                console.log('kurs sie zmiejszył');
+            } else if ( kursBtc < ostatniaWartosc) {
+                console.log('kurs się zwiększył');
+            }
+
+            kursBtc = ostatniaWartosc;
+
+
+
+            // let previousValue = [];
+            // let currentValue = [];
+
+                // $.each(btc, function(key, value){                               // Tworzy obiekt
+                //     let currentValue = value.last_trade_price;
+
+                    // let previousValue  = [value.last_trade_price];
+                    // let currentValue = [];
+                    // currentValue.splice(0, 1, previousValue);
+
+                    // for (i in btc) {
+                    //     previousValue.push(btc[i].last_trade_price);
+                    // }
+
+                    // let newPreviouseValue = previousValue;
+
+                    // let cc = [].concat(newPreviouseValue, previousValue);
+
+
+                    // let previousValue  = [];
+                    // let currentValue = [];
+
+                    // previousValue.push(value.last_trade_price);
+                    // currentValue.concat(previousValue);
+
+                    // let old = storedValue.findIndex(x => x === currentValue) - 1;
+
+
+                    // console.log(previousValue);
+                    // console.log(cc);
+                    // console.log(currentValue);
+
+
+                    // console.log(btc);
+                    // console.log(value);
+                    
+                    // });
+                
+
             
             
         });
@@ -33,11 +86,11 @@ $(document).ready(function(){
 });
 
 
-// let high = $("<p></p>").text(value.last_trade_price).append($('#btcHigh'));
-//                     let low = $("<p></p>").text(value.price_24h);
-        
-//                     $('.btcHigh').append(high);
-//                     $('.btcLow').append(low);
+
+
+
+
+
 
 
 // last_trade_price: 19341.96​
